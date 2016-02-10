9619413a7288af5d3d1c27b8ca6013ba9f827d59
Zaid Hakak <zaid@gtugs.org>
2016-02-10 15:22:46 +0530

    Visualize the speed data using matplotlib.

--- parse.py
+++ parse.py
@@ -25,7 +25,40 @@ def speed_ans(self, data_file):
 # Example list an dict
 dict = json_data
 # Sort the dict
-output = sorted(dict, key=itemgetter(*['Speed']), reverse = True)
-natsort.natsorted(dict, key=itemgetter(*['Speed']))
+#sorted(dict, key=itemgetter(*['Speed']), reverse = True)
+output = natsort.natsorted(dict, key=itemgetter(*['Speed']), reverse = True)
 
 pprint.pprint(output)
+
+def visualize_type(output):
+    """Visualize data by category in a bar graph"""
+
+    #This returns a dict where it sums the total per Category.
+    counter = Counter(item["Speed"] for item in output)
+
+    # Set the labels which are based on the keys of our counter.
+    labels = tuple(counter.key("Speed"))
+
+    # Set where the labels hit the x-axis
+    xlocations = np.arange(len(labels)) + 0.5
+
+    # Width of each bar
+    width = 0.5
+
+    # Assign data to a bar plot
+    plt.bar(xlocations, counter.values(), width=width)
+
+    # Assign labels and tick location to x- and y-axis
+    plt.xticks(xlocations + width / 2, labels, rotation=90)
+    plt.yticks(range(0, max(counter.values()), 5))
+
+    # Give some more room so the labels aren't cut off in the graph
+    plt.subplots_adjust(bottom=0.4)
+
+    # Make the overall graph/figure larger
+    plt.rcParams['figure.figsize'] = 12, 8
+
+    # Save the graph!
+    plt.savefig("Graph.png")
+
+    plt.clf()
