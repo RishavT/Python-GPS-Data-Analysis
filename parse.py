58ab1a300d0b41dba251c4b97f71f2dfe166f825
Rishav Thakker <rishav@Rishavs-iMac.local>
2016-02-16 00:36:14 +0530

    1. Wrote a function to calculate acceleration at each point. 2. Added the files output* to gitignore.

--- parse.py
+++ parse.py
@@ -59,7 +59,14 @@ def visualize_type(output):
 
     plt.clf()
 
-
+def differentiate(v1, v2, t1, t2):
+    """Method to differentiate velocity and get acceleration.
+    Uses the approximation:
+    dy/dx = (y2-y1)/(x2-x1) if x1 is almost equal to x2.
+    """
+    dx = v2-v1
+    dt = t2-t1
+    return dx/dt
 
 if __name__ == '__main__':
     with open(data_file) as f:
@@ -68,12 +75,42 @@ if __name__ == '__main__':
     sorted_waypoints = natsort.natsorted(waypoints, key=itemgetter(*['Speed']), reverse = True)
     #pprint.pprint(sorted_waypoints)
 
-    for e in sorted_waypoints:
-        for k, v in e.items():
-            if (k == 'Speed' and v >= 5): #Set the threshold to 5 mps for speed anything less will be ignored.
-                print k, v
-                for k, v in e.items():
-                    if (k == 'Timestamp'):
-                        print k, v
+    print len(sorted_waypoints)
+    for i, e in enumerate(sorted_waypoints):
+        # for k, v in e.items():
+        #     if (k == 'Speed' and v >= 5): #Set the threshold to 5 mps for speed anything less will be ignored.
+        #         print k, v
+        #         for k, v in e.items():
+        #             if (k == 'Timestamp'):
+        #                 print k, v
+        if e['Speed'] >= 5:
+            print 'Speed', e['Speed']
+            print 'Timestamp', e['Timestamp']
+
+        # Calculate the acceleration at that point
+        # Since acceleration = dv/dt (differentiation of velocity w.r.t time)
+        # We'll call the differentiate function.
+
+        # Since we're using the enumerate function, e is equivalent to
+        # sorted_waypoints[i]. Hence the earlier value would be sorted_waypoints[i-1]
+        # and the next value would be sorted_waypoints[i+1].
+        # We cannot get acceleration for either the starting or the ending point,
+        # Depending on whether we use i-1 or i+1. Both should be good if the
+        # timestamps are small enough.
+
+        # For now we'll use [i-1], so the starting point will not have acceleration
+        # defined.
+
+        if i==0:
+            continue
+
+        previous_e = sorted_waypoints[i-1]
+        e['Acceleration'] = differentiate(v2=e['Speed'],
+                                          v1=previous_e['Speed'],
+                                          t2=e['Timestamp'],
+                                          t1=previous_e['Timestamp'])
+
+        print 'Acceleration', e['Acceleration']
+
 
     visualize_type(sorted_waypoints)
