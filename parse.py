f99abc7c032cce22fd0e543f2ce28ba859c2ca84
Rishav Thakker <rishav@Rishavs-iMac.local>
2016-02-16 01:57:54 +0530

    Wrote the function to extract good points from our data. Have not commented it properly yet, will do that soon.

--- parse.py
+++ parse.py
@@ -68,32 +68,30 @@ def differentiate(v1, v2, t1, t2):
     dt = t2-t1
     return dx/dt
 
-if __name__ == '__main__':
-    with open(data_file) as f:
-        waypoints = json.load(f)
-
-    sorted_waypoints = natsort.natsorted(waypoints, key=itemgetter(*['Speed']), reverse = True)
-    #pprint.pprint(sorted_waypoints)
-
-    print len(sorted_waypoints)
-    for i, e in enumerate(sorted_waypoints):
-        # for k, v in e.items():
-        #     if (k == 'Speed' and v >= 5): #Set the threshold to 5 mps for speed anything less will be ignored.
-        #         print k, v
-        #         for k, v in e.items():
-        #             if (k == 'Timestamp'):
-        #                 print k, v
+def display(waypoints):
+    """Function to print our waypoints list"""
+    for i, e in enumerate(waypoints):
         if e['Speed'] >= 5:
             print 'Speed', e['Speed']
             print 'Timestamp', e['Timestamp']
+            if 'Acceleration' in e:
+                print 'Acceleration', e['Acceleration']
+
+def calculate_acceleration(waypoints):
+    """Function to calculate the acceleration for each point in our list"""
+    waypoints_with_acceleration = []
+    for i, e in enumerate(waypoints):
+        if e['Speed'] >= 5:
+            # print 'Speed', e['Speed']
+            # print 'Timestamp', e['Timestamp']
 
             # Calculate the acceleration at that point
             # Since acceleration = dv/dt (differentiation of velocity w.r.t time)
             # We'll call the differentiate function.
 
             # Since we're using the enumerate function, e is equivalent to
-            # sorted_waypoints[i]. Hence the earlier value would be sorted_waypoints[i-1]
-            # and the next value would be sorted_waypoints[i+1].
+            # waypoints[i]. Hence the earlier value would be waypoints[i-1]
+            # and the next value would be waypoints[i+1].
             # We cannot get acceleration for either the starting or the ending point,
             # Depending on whether we use i-1 or i+1. Both should be good if the
             # timestamps are small enough.
@@ -104,13 +102,65 @@ if __name__ == '__main__':
             if i==0:
                 continue
 
-            previous_e = sorted_waypoints[i-1]
+            previous_e = waypoints[i-1]
+            # Save the acceleration inside the dict so that it can be accessed later.
+            # outside this loop.
+            # Hence using the original varuable instead of `e`
             e['Acceleration'] = differentiate(v2=e['Speed'],
                                               v1=previous_e['Speed'],
                                               t2=e['Timestamp'],
                                               t1=previous_e['Timestamp'])
 
-            print 'Acceleration', e['Acceleration']
+            # print 'Acceleration', e['Acceleration']
+            waypoints_with_acceleration.append(e)
+    return waypoints_with_acceleration
+
+def extract_good_points(waypoints):
+    i = 1
+    good_points = []
+    acceleration_treshold = 0
+    while i < len(waypoints):
+
+        if waypoints[i]['Acceleration'] < acceleration_treshold:
+            # This means he's slowing down
+            # We'll loop through the upcoming time slots
+            # and take the last 3 seconds of slowing down/ constant speed.
+            # Basically if it's
+            # slowdown/slowdown/slowdown/slowdown/constant/constant/speedup/speedup
+            # We'll only keep slowdown/constant/constant/speedup/speedup
+
+            old_i = 0
+            is_done = False
+            while i < len(waypoints) and not is_done:
+                if waypoints[i]['Acceleration'] > acceleration_treshold:
+                    # This implies he's accelerating again.
+                    # We take at max last 3 points of slow speed from now.
+                    # This is the same as minimum of 3, total slow speed duration.
+                    j = i - min(i - 3, i - old_i)
+                    while j <= i:
+                        good_points.append(waypoints[j])
+                        j += 1
+                    is_done = True
+                i += 1
+        else:
+            # This means the driver is constant or accelerating
+            # So we want this
+            good_points.append(waypoints[i])
+        i += 1
+        # print i
+
+    return good_points
 
 
+if __name__ == '__main__':
+    with open(data_file) as f:
+        waypoints = json.load(f)
+
+    sorted_waypoints = natsort.natsorted(waypoints, key=itemgetter(*['Timestamp']), reverse = False)
+    waypoints_with_acceleration = calculate_acceleration(sorted_waypoints)
+    # display(waypoints_with_acceleration)
+    good_points = extract_good_points(waypoints_with_acceleration)
+    display(good_points)
+
+    #pprint.pprint(sorted_waypoints)
     visualize_type(sorted_waypoints)
